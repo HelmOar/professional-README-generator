5bfb5f6565a37c844c0e2c3871ff707821456ef7
caroline <chelmore1@bigpond.com>
2023-04-11 08:15:16 +1000

    badges added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
+
 // TODO: Create an array of questions for user input
 const questions = [
 
@@ -27,7 +28,7 @@ const questions = [
 
     {
         type: "input",
-        name: "problem-solved",
+        name: "problem_solved",
         message: "What problem does it solve?"
     }, 
    
@@ -36,24 +37,46 @@ const questions = [
         name: "learnings",
         message: "What did you learn?"
     }, 
-    // {
-    //     type: "input",
-    //     name: "installation",
-    //     message: "What are the steps required to install your project?"
-    // }, {
-    //     type: "input",
-    //     name: "usage",
-    //     message: "What are the instructions for use?"
-    // }, {
-    //     type: "input",
-    //     name: "credits",
-    //     message: "Who are your collaborators?"
-    // }, {
-    //     type: "list",
-    //     name: "license",
-    //     message: "Do you have a lisence?",
-    //     choices: ["MIT", "Apache", "GPL", "None"]
-    // },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is required to install your project?"
+    }, 
+    {
+        type: "input",
+        name: "usage",
+        message: "What are the instructions for use?"
+    }, 
+    //
+    {
+        type: "input",
+        name: "credits",
+        message: "Who are your collaborators?"
+    }, 
+    
+    {
+        type: "input",
+        name: "tests",
+        message: "Describe any tests you run for your project."
+    }, 
+    {
+        type: "list",
+        name: "license",
+        message: "Do you have a lisence?",
+        choices: ["MIT", "Apache", "GPL", "None"]
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    }, 
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github address?"
+    }, 
 
 ];
 // TODO: Create a function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,55 @@
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to render badge
+function renderBadge(license) {
+  if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n`;  }
+ 
+  else if (license === "Apache") { 
+    `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n`;
+    
+  }
+  else if (license === "GPL") {
+    `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n`;
+  }
+  else if (license === "None") {
+    return '';
+  
+  }
+}
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {
+// Function to render link
+
+//   if (license !== "None") {
+//     return (
+//       `\n* [License](#license)\n`
+//     )
+//   }
+//   return ''
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+// Function to render license section
+  if (license !== "None") {
+    return (
+      `## License 📛
+      Copyright © ${license}. All rights reserved. 
+      
+      Licensed under the ${license} license.`
+
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -16,13 +57,43 @@ function generateMarkdown(data) {
 
 ## Description
 ${data.description}
-
-## Project Motivation
-${data.problem-solved}
-
-## Discoveries and Learnings
+### Motivation
+${data.motivation}
+### Problem Solved
+${data.problem_solved}
+### Learnings
 ${data.learnings}
+
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+##Contributing
+${data.credits}
+
+## Tests
+${data.tests}
+
+## License
+${data.license}
+
+## Questions
+If you have any questions, please contact me at ${data.email}.
+You can also find more of my work at [${data.github}]
+
 `;
 }
 
 module.exports = generateMarkdown;
+
